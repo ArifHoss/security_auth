9c88f4f7a971f1dc75e35f92e2b8d92118a08364
Arif Hossain <hossaininfo@ymail.com>
2024-03-27 02:52:07 +0100

    created multiple user, commented out user in application.properties

--- src/main/java/com/spring/security_auth/security/ProjectWebSecurityConfiguration.java
+++ src/main/java/com/spring/security_auth/security/ProjectWebSecurityConfiguration.java
@@ -3,6 +3,9 @@ package com.spring.security_auth.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -24,5 +27,18 @@ public class ProjectWebSecurityConfiguration{
         return http.build();
     }
 
-
+    @Bean
+    InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("arif")
+                .password("1234")
+                .roles("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("sumon")
+                .password("1234")
+                .roles("user")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
 }
