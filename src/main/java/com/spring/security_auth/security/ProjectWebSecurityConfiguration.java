dd4335534a478b547d759dd3a7c78ac8ed7e314a
Arif Hossain <hossaininfo@ymail.com>
2024-03-28 03:01:41 +0100

    created Custom UserDetailsService, implemented 'loadUSerByUsername' method, now a customers username and password comes direct from database

--- src/main/java/com/spring/security_auth/security/ProjectWebSecurityConfiguration.java
+++ src/main/java/com/spring/security_auth/security/ProjectWebSecurityConfiguration.java
@@ -3,9 +3,8 @@ package com.spring.security_auth.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -26,7 +25,7 @@ public class ProjectWebSecurityConfiguration{
         http.httpBasic(withDefaults());
         return http.build();
     }
-
+/*
     @Bean
     InMemoryUserDetailsManager userDetailsManager() {
         UserDetails admin = User.withDefaultPasswordEncoder()
@@ -40,5 +39,10 @@ public class ProjectWebSecurityConfiguration{
                 .roles("user")
                 .build();
         return new InMemoryUserDetailsManager(admin, user);
+    }*/
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
